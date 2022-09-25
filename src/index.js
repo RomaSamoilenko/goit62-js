23f787d2217709a453898582ab92df3f57c64854
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2022-09-25 22:50:51 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+// console.log('hello');
+// const yearOfBirth = 2006;
+// console.log(yearOfBirth);
+function getNameLength(name) {
+    const message = `Name ${name} is ${length} characters long`; 
+  
+    return message;
+  }
